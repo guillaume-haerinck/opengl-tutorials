330fee962ad81f36a5c435e004e131fb3294389d
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-11-11 22:06:15 +0100

    Start ex 04

---
+++ src/debug-04/main.cpp
@@ -0,0 +1,166 @@
+#include <SDL2/SDL.h>
+#include <glad/glad.h>
+#include <spdlog/spdlog.h>
+#include <debug_break/debug_break.h>
+
+#include "common/app.h"
+#include "common/gl-exception.h"
+
+int main(int argc, char *argv[]) {
+    App app;
+
+    glClearColor(1, 0, 1, 1);
+
+    // ------------------ Vertex Buffer 1
+    unsigned int posVB;
+    {
+        float positions[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f,  0.5f, 0.0f
+        };
+        
+        GLCall(glGenBuffers(1, &posVB));
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, posVB));
+        GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW));
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    }
+
+    // ------------------ Vertex Buffer 2
+    unsigned int colorVB;
+    {
+        float colors[] = {
+            1, 0, 0,
+            0, 1, 0,
+            0, 0, 1
+        };
+        
+        GLCall(glGenBuffers(1, &colorVB));
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, colorVB));
+        GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW));
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    }
+    
+
+    // ------------------ Vertex Array
+    unsigned int vao;
+    {
+        GLCall(glGenVertexArrays(1, &vao));
+        GLCall(glBindVertexArray(vao));
+
+        // Vertex input description
+        {
+            GLCall(glEnableVertexAttribArray(0));
+            GLCall(glBindBuffer(GL_ARRAY_BUFFER, posVB));
+            GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL));
+
+            GLCall(glEnableVertexAttribArray(1));
+            GLCall(glBindBuffer(GL_ARRAY_BUFFER, colorVB));
+            GLCall(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL));
+        }
+        
+        GLCall(glBindVertexArray(0));
+    }
+    
+
+    // ------------------ Vertex shader
+    unsigned int vs;
+    int success;
+    char infoLog[512];
+    {
+        const char* vsSource = R"(#version 330 core
+            layout (location = 0) in vec3 aPos;
+            layout (location = 1) in vec3 aColor;
+
+            out vec3 vColor;
+
+            void main() {
+                vColor = aColor;
+                gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+            }
+        )";
+        
+        vs = glCreateShader(GL_VERTEX_SHADER);
+        GLCall(glShaderSource(vs, 1, &vsSource, NULL));
+        GLCall(glCompileShader(vs));
+
+        // Check compilation
+        GLCall(glGetShaderiv(vs, GL_COMPILE_STATUS, &success));
+        if (!success) {
+            GLCall(glGetShaderInfoLog(vs, 512, NULL, infoLog));
+            spdlog::critical("[VertexShader] Compilation failed : {}", infoLog);
+            debug_break();
+        }
+    }
+
+    // ------------------ Fragment shader
+    unsigned int fs;
+    {
+        const char* fsSource = R"(#version 330 core
+            out vec4 FragColor;
+
+            in vec3 vColor;
+
+            void main() {
+                FragColor = vec4(vColor, 1.0f);
+            } 
+        )";
+        
+        fs = glCreateShader(GL_FRAGMENT_SHADER);
+        GLCall(glShaderSource(fs, 1, &fsSource, NULL));
+        GLCall(glCompileShader(fs));
+
+        // Check compilation
+        int success;
+        GLCall(glGetShaderiv(fs, GL_COMPILE_STATUS, &success));
+        if (!success) {
+            GLCall(glGetShaderInfoLog(fs, 512, NULL, infoLog));
+            spdlog::critical("[FragmentShader] Compilation failed : {}", infoLog);
+            debug_break();
+        }
+    }
+
+    // ------------------ Pipeline
+    unsigned int pipeline;
+    {
+        pipeline = glCreateProgram();
+        GLCall(glAttachShader(pipeline, vs));
+        GLCall(glAttachShader(pipeline, fs));
+        GLCall(glLinkProgram(pipeline));
+
+        // Check compilation
+        GLCall(glGetProgramiv(pipeline, GL_LINK_STATUS, &success));
+        if (!success) {
+            GLCall(glGetProgramInfoLog(pipeline, 512, NULL, infoLog));
+            spdlog::critical("[Pipeline] Link failed : {}", infoLog);
+            debug_break();
+        }
+
+        // Delete useless data
+        GLCall(glDeleteShader(vs));
+        GLCall(glDeleteShader(fs));
+    }
+    
+
+    while (app.isRunning()) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+            case SDL_QUIT: app.exit();
+
+            default: break;
+            };
+        }
+
+        app.beginFrame();
+
+        // Draw call
+        GLCall(glUseProgram(pipeline));
+        GLCall(glBindVertexArray(vao));
+        GLCall(glDrawArrays(GL_TRIANGLES, 0, 3));
+
+        app.endFrame();
+    }
+    
+    return 0;
+}
